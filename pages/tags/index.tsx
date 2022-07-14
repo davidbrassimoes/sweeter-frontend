278de240f81978ad7b6e1cd6529e59d96eadb4c2
davidbrassimoes <david.simoes.1994@gmail.com>
2022-07-14 15:35:13 +0100

    added followTag and followUser

--- pages/tags/index.tsx
+++ pages/tags/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { FormEvent, useEffect, useState } from "react"
 import SideBar from "../../components/sidebar"
 import { useAuth } from '../../hooks/useAuth'
 import { api } from '../../services/api'
@@ -8,6 +8,20 @@ export default function Tag() {
     const [tags, setTags] = useState(null)
     const [isLoading, setLoading] = useState(false)
     const { user } = useAuth()
+
+    async function followHandler(tag) {
+        const { data } = await api.get(`users/${user.id}`)
+        const myUserFollows = data[0].followsTag
+        console.log("myUserFollows", myUserFollows);
+        const myUserFollowsId = myUserFollows.map(x => x.id)
+        console.log("myUserFollowsId", myUserFollowsId);
+
+        if (myUserFollowsId.includes(tag.id)) { return alert("Already Followed") }
+
+        const res = await api.put(`users/${user.id}`, { followsTag: [...myUserFollows, { "id": tag.id }] })
+        return console.log(res)
+    }
+
     useEffect(() => {
         setLoading(true)
         api.get('/tags').then(({ data }) => {
@@ -30,8 +44,7 @@ export default function Tag() {
                     <p className="text-2xl" >
                         <i>#{tag.content}</i>
                     </p>
-                    <span>&middot;</span>
-                    <button onClick={() => console.log(`I Wanna Follow #${tag.content}`)} className="sweet-button">
+                    <button onClick={() => followHandler(tag)} className="sweet-button">
                         Follow
                     </button>
                 </div>
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,11 +1,29 @@
 import { useEffect, useState } from "react"
 import SideBar from "../../components/sidebar"
 import { api } from '../../services/api'
+import { useAuth } from '../../hooks/useAuth'
+
 
 
 export default function User() {
     const [users, setUsers] = useState(null)
     const [isLoading, setLoading] = useState(false)
+    const myId = useAuth().user.id
+
+    async function followHandler(user) {
+        const { data } = await api.get(`users/${myId}`)
+        const myUserFollows = data[0].followsUser
+        console.log("myUserFollows", myUserFollows);
+        const myUserFollowsId = myUserFollows.map(x => x.id)
+        console.log("myUserFollowsId", myUserFollowsId);
+        console.log(user);
+
+        if (myUserFollowsId.includes(user.id)) { return alert("Already Followed") }
+
+        const res = await api.put(`users/${myId}`, { followsUser: [...myUserFollows, { "id": user.id }] })
+        return console.log(res)
+    }
+
     useEffect(() => {
         setLoading(true)
         api.get('/users').then(({ data }) => {
@@ -31,7 +49,7 @@ export default function User() {
                     <p className="text-xl">
                         <i>{user.bio}</i>
                     </p>
-                    <button onClick={() => console.log(`I Wanna Follow @${user.username}`)} className="sweet-button">
+                    <button onClick={() => followHandler(user)} className="sweet-button">
                         Follow
                     </button>
                 </div>
